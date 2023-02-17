a3551d2b4cd8011b6358d3b913492fdb3a74af4f
Daniel Buck <dbuck087@gmail.com>
2023-02-17 23:19:28 +0000

    Added sum_to_n code first attempt and iteration

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,16 +3,21 @@
 # Part 1
 
 def sum(array)
+  # calls built in sum() method of the array object and returns the value
   return array.sum()
 end
 
 def max_2_sum(array)
+#built in sort method for arrays. the '!' command overwrites orignal array with new sorted array
   array.sort!
 
+  #check if array is empty, if so return 0
   if array.empty?
     sum = 0
+  #check if array has only one entry, if so return that one entry.  
   elsif array[-2] == nil
     sum = array[-1]
+  #if neither of the previous are true than add the last two elements in the array and return the value
   else
     sum = array[-1] + array[-2]
   end
@@ -20,8 +25,15 @@ def max_2_sum(array)
   return sum
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n? (array, num)
+  bool = false
+  array.each do |i|
+    array.each do |j|
+      if (array[i] + array[j]) == num
+        bool = true
+      end
+    end
+  end
 end
 
 # Part 2
