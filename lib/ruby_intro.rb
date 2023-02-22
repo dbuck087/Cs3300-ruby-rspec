0332d993b2067d51dd69aefc948139c023521c28
Daniel Buck <dbuck087@gmail.com>
2023-02-22 03:08:37 +0000

    added starts with constanant code

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -44,7 +44,12 @@ def hello(name)
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if (s[0] == 'A' || s[0] == 'a' || s[0] == 'E'|| s[0] == 'e'||
+    s[0] == 'I'||s[0] == 'i'||s[0] == 'O'||s[0] == 'o'||s[0] == 'U'||s[0] == 'u')
+    return true
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
