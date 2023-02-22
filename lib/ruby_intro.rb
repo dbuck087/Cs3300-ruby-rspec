66eccc2005b6eda6184669d24ae06465363ef2c7
Daniel Buck <dbuck087@gmail.com>
2023-02-22 02:52:51 +0000

    trying code from stackoverflow

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -29,7 +29,7 @@ def sum_to_n? (array, num)
 
   # return false if array.empty? || array[-2] == nil
 
-  # return true if array.permutation(2).any? {|a,b| a+b==n}
+  (array.empty? && num.zero?) || array.permutation(2).any? {|a,b| a+b==num}
     
 end
 
