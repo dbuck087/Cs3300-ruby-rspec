d27728fe5a63a5aa70cfe8e0ed467ec85bb2ef39
Daniel Buck <dbuck087@gmail.com>
2023-02-22 03:10:20 +0000

    fixed returns on start ith constanant

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -46,9 +46,9 @@ end
 def starts_with_consonant? s
   if (s[0] == 'A' || s[0] == 'a' || s[0] == 'E'|| s[0] == 'e'||
     s[0] == 'I'||s[0] == 'i'||s[0] == 'O'||s[0] == 'o'||s[0] == 'U'||s[0] == 'u')
-    return true
-  else
     return false
+  else
+    return true
   end
 end
 
